71b42b440792f181d539435ed9d979d17ae5e16f
Baker Bokorney <baker_bokorney@ultimatesoftware.com>
2017-07-12 06:27:09 -0400

    No need to symlink vim colors in the script

--- symlink.sh
+++ symlink.sh
@@ -6,5 +6,3 @@ for file in $(echo .bashrc .inputrc .vim .vimrc); do
    ln -fs ~/dotfiles/$file ~/$file
 done
 
-# Link vim colors
-ln -s ~/dotfiles/.vim/bundle/vim-colorschemes/colors ~/dotfiles/.vim/colors
